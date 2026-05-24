5ed3c5abe6882e7c3130ba6269f4b15c6cf56126
Hector Alvarez <alvarez.hectorm@outlook.com>
2026-05-23 21:11:51 -0500

    design telegram-optimized core data models and admin layouts

---
+++ api/store/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import TelegramUser, Category, Product, Cart, CartItem, Order, OrderItem
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id', 'username', 'first_name', 'is_active', 'created_at')
+    search_fields = ('telegram_id', 'username', 'first_name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'is_visible')
+    list_filter = ('category', 'is_visible')
+    search_fields = ('name', 'description')
+
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
